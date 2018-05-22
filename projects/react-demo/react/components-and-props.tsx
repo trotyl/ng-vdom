2398fe4b9839196276f47028d65325d9e57c47cb
Trotyl <trotyl@qq.com>
2018-05-22 15:33:11 +0800

    docs: complete components-and-props demo

--- projects/react-demo/react/components-and-props.tsx
+++ projects/react-demo/react/components-and-props.tsx
@@ -35,5 +35,107 @@ function demo_1() {
   )
 }
 
+function demo_2() {
+  function formatDate(date: Date) {
+    return date.toLocaleDateString()
+  }
+
+  function Comment(props: any) {
+    return (
+      <div className="Comment">
+        <div className="UserInfo">
+          <img
+            className="Avatar"
+            src={props.author.avatarUrl}
+            alt={props.author.name}
+          />
+          <div className="UserInfo-name">
+            {props.author.name}
+          </div>
+        </div>
+        <div className="Comment-text">{props.text}</div>
+        <div className="Comment-date">
+          {formatDate(props.date)}
+        </div>
+      </div>
+    )
+  }
+
+  const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+      name: 'Hello Kitty',
+      avatarUrl: 'http://placekitten.com/g/64/64',
+    },
+  }
+
+  render(
+    <Comment
+      date={comment.date}
+      text={comment.text}
+      author={comment.author}
+    />,
+    generate()
+  )
+}
+
+function demo_3() {
+  function formatDate(date: Date) {
+    return date.toLocaleDateString()
+  }
+
+  function Avatar(props: any) {
+    return (
+      <img
+        className="Avatar"
+        src={props.user.avatarUrl}
+        alt={props.user.name}
+      />
+    )
+  }
+
+  function UserInfo(props: any) {
+    return (
+      <div className="UserInfo">
+        <Avatar user={props.user} />
+        <div className="UserInfo-name">{props.user.name}</div>
+      </div>
+    )
+  }
+
+  function Comment(props: any) {
+    return (
+      <div className="Comment">
+        <UserInfo user={props.author} />
+        <div className="Comment-text">{props.text}</div>
+        <div className="Comment-date">
+          {formatDate(props.date)}
+        </div>
+      </div>
+    )
+  }
+
+  const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+      name: 'Hello Kitty',
+      avatarUrl: 'http://placekitten.com/g/64/64',
+    },
+  }
+
+  render(
+    <Comment
+      date={comment.date}
+      text={comment.text}
+      author={comment.author}
+    />,
+    generate()
+  )
+}
+
 demo_0()
 demo_1()
+demo_2()
+demo_3()
--- projects/react-demo/utils/element.ts
+++ projects/react-demo/utils/element.ts
@@ -1,5 +1,7 @@
 export function generate(): HTMLElement {
   const element = document.createElement('div')
   document.body.appendChild(element)
+  const hr = document.createElement('hr')
+  document.body.appendChild(hr)
   return element
 }
